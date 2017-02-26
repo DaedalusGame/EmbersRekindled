68908d731c6253c3168e80f7a3537240b5a135ba
gloomshroud <brandondstewart@gmail.com>
2017-02-26 13:50:40 -0600

    Gloomshroud: Added mithril ingots, nuggets, plates, and blocks.

--- src/main/java/teamroots/embers/RegistryManager.java
+++ src/main/java/teamroots/embers/RegistryManager.java
@@ -54,12 +54,12 @@ public class RegistryManager {
 	public static ToolMaterial tool_mat_tyrfing, tool_mat_copper, tool_mat_silver, tool_mat_lead, tool_mat_dawnstone;
 	public static ArmorMaterial armor_mat_ashen_cloak;
 	
-	public static Block field_chart, catalyzer, combustor, reactor, archaic_tile, archaic_edge, wrapped_sealed_planks, structure_marker, boiler, ember_injector, seed, breaker, vacuum, sealed_planks, ore_quartz, auto_hammer, dawnstone_anvil, archaic_light, archaic_bricks, glow, beam_cannon, item_transfer, alchemy_tablet, alchemy_pedestal, knowledge_table, cinder_plinth, ashen_tile, stairs_ashen_tile, wall_ashen_tile, ashen_tile_slab, ashen_tile_slab_double, ashen_stone, ashen_brick, stairs_ashen_stone, wall_ashen_stone, ashen_stone_slab, ashen_stone_slab_double, stairs_ashen_brick, wall_ashen_brick, ashen_brick_slab, ashen_brick_slab_double, block_caminite_brick_slab, block_caminite_brick_slab_double, charger, crystal_cell, advanced_edge, ember_relay, beam_splitter, block_lantern, ember_gauge, item_gauge, fluid_gauge, large_tank, item_dropper, heat_coil, wall_caminite_brick, block_dawnstone, mixer, stone_edge, ember_activator, mech_core, stairs_caminite_brick, mech_accessor, ember_bore, mech_edge, item_pump, item_pipe, block_oven, stamp_base, stamper, block_caminite_large_brick, bin, copper_cell, deep_line, ember_emitter, ember_receiver, block_furnace, pump, block_copper, block_lead, block_silver, ore_copper, ore_lead, ore_silver, block_caminite_brick, block_tank, pipe;
+	public static Block field_chart, catalyzer, combustor, reactor, archaic_tile, archaic_edge, wrapped_sealed_planks, structure_marker, boiler, ember_injector, seed, breaker, vacuum, sealed_planks, ore_quartz, auto_hammer, dawnstone_anvil, archaic_light, archaic_bricks, glow, beam_cannon, item_transfer, alchemy_tablet, alchemy_pedestal, knowledge_table, cinder_plinth, ashen_tile, stairs_ashen_tile, wall_ashen_tile, ashen_tile_slab, ashen_tile_slab_double, ashen_stone, ashen_brick, stairs_ashen_stone, wall_ashen_stone, ashen_stone_slab, ashen_stone_slab_double, stairs_ashen_brick, wall_ashen_brick, ashen_brick_slab, ashen_brick_slab_double, block_caminite_brick_slab, block_caminite_brick_slab_double, charger, crystal_cell, advanced_edge, ember_relay, beam_splitter, block_lantern, ember_gauge, item_gauge, fluid_gauge, large_tank, item_dropper, heat_coil, wall_caminite_brick, block_dawnstone, mixer, stone_edge, ember_activator, mech_core, stairs_caminite_brick, mech_accessor, ember_bore, mech_edge, item_pump, item_pipe, block_oven, stamp_base, stamper, block_caminite_large_brick, bin, copper_cell, deep_line, ember_emitter, ember_receiver, block_furnace, pump, block_copper, block_lead, block_silver, block_mithril, ore_copper, ore_lead, ore_silver, block_caminite_brick, block_tank, pipe;
 	public static Block block_molten_astralite, block_molten_dawnstone, block_molten_umber_steel, block_molten_gold, block_molten_copper, block_molten_lead, block_molten_silver, block_molten_iron;
 	
 	public static Fluid fluid_molten_astralite, fluid_molten_dawnstone, fluid_molten_umber_steel, fluid_molten_gold, fluid_molten_copper, fluid_molten_lead, fluid_molten_silver, fluid_molten_iron;
 	
-	public static Item wildfire_core, ember_cluster, adhesive, tyrfing, isolated_materia, archaic_brick, ancient_motive_core, ashen_cloth, glimmer_shard, glimmer_lamp, inflictor_gem, ashen_cloak_head, ashen_cloak_chest, ashen_cloak_legs, ashen_cloak_boots, aster, shard_aster, alchemic_waste, aspectus_iron, aspectus_copper, aspectus_dawnstone, aspectus_lead, aspectus_silver, golems_eye, dust_ash, grandhammer, pickaxe_clockwork, axe_clockwork, staff_ember, ignition_cannon, ember_jar, ember_cartridge, pickaxe_copper, axe_copper, shovel_copper, hoe_copper, sword_copper, pickaxe_silver, axe_silver, shovel_silver, hoe_silver, sword_silver, pickaxe_lead, axe_lead, shovel_lead, hoe_lead, sword_lead, pickaxe_dawnstone, axe_dawnstone, shovel_dawnstone, hoe_dawnstone, sword_dawnstone, debug, plate_gold, plate_iron, plate_caminite_raw, stamp_bar_raw, stamp_plate_raw, stamp_flat_raw, nugget_dawnstone, plate_copper, plate_lead, plate_silver, plate_dawnstone, nugget_iron, ingot_astralite, ingot_dawnstone, ingot_umber_steel, crystal_ember, shard_ember, stamp_bar, stamp_plate, stamp_flat, tinker_hammer, ember_detector, ingot_copper, ingot_silver, ingot_lead, nugget_copper, nugget_silver, nugget_lead, brick_caminite, blend_caminite, plate_caminite;
+	public static Item wildfire_core, ember_cluster, adhesive, tyrfing, isolated_materia, archaic_brick, ancient_motive_core, ashen_cloth, glimmer_shard, glimmer_lamp, inflictor_gem, ashen_cloak_head, ashen_cloak_chest, ashen_cloak_legs, ashen_cloak_boots, aster, shard_aster, alchemic_waste, aspectus_iron, aspectus_copper, aspectus_dawnstone, aspectus_lead, aspectus_silver, golems_eye, dust_ash, grandhammer, pickaxe_clockwork, axe_clockwork, staff_ember, ignition_cannon, ember_jar, ember_cartridge, pickaxe_copper, axe_copper, shovel_copper, hoe_copper, sword_copper, pickaxe_silver, axe_silver, shovel_silver, hoe_silver, sword_silver, pickaxe_lead, axe_lead, shovel_lead, hoe_lead, sword_lead, pickaxe_dawnstone, axe_dawnstone, shovel_dawnstone, hoe_dawnstone, sword_dawnstone, debug, plate_gold, plate_iron, plate_caminite_raw, plate_mithril, stamp_bar_raw, stamp_plate_raw, stamp_flat_raw, nugget_dawnstone, plate_copper, plate_lead, plate_silver, plate_dawnstone, nugget_iron, nugget_mithril, ingot_astralite, ingot_dawnstone, ingot_umber_steel, ingot_mithril, crystal_ember, shard_ember, stamp_bar, stamp_plate, stamp_flat, tinker_hammer, ember_detector, ingot_copper, ingot_silver, ingot_lead, nugget_copper, nugget_silver, nugget_lead, brick_caminite, blend_caminite, plate_caminite;
 	
 	public static DamageSource damage_ember;
 	
@@ -93,6 +93,8 @@ public class RegistryManager {
 		blocks.add(block_lead = (new BlockBase(Material.ROCK,"block_lead",true)).setHarvestProperties("pickaxe", 2).setHardness(1.6f).setLightOpacity(16));
 		blocks.add(block_silver = (new BlockBase(Material.ROCK,"block_silver",true)).setHarvestProperties("pickaxe", 2).setHardness(1.6f).setLightOpacity(16));
 		blocks.add(block_dawnstone = (new BlockBase(Material.ROCK,"block_dawnstone",true)).setHarvestProperties("pickaxe", 2).setHardness(1.6f).setLightLevel(0.0625f).setLightOpacity(16));
+		// Gloomshroud - Add Mithril Block. May need property adjustment.
+		blocks.add(block_mithril = (new BlockBase(Material.ROCK, "block_mithril", true)).setHarvestProperties("pickaxe", 2).setHardness(1.6f).setLightLevel(0.0625f).setLightOpacity(16));
 		blocks.add(ore_copper = (new BlockBase(Material.ROCK,"ore_copper",true)).setIsFullCube(true).setIsOpaqueCube(true).setHarvestProperties("pickaxe", 1).setHardness(1.8f).setLightOpacity(16));
 		blocks.add(ore_lead = (new BlockBase(Material.ROCK,"ore_lead",true)).setIsFullCube(true).setIsOpaqueCube(true).setHarvestProperties("pickaxe", 2).setHardness(2.5f).setLightOpacity(16));
 		blocks.add(ore_silver = (new BlockBase(Material.ROCK,"ore_silver",true)).setIsFullCube(true).setIsOpaqueCube(true).setHarvestProperties("pickaxe", 2).setHardness(2.5f).setLightOpacity(16));
@@ -187,17 +189,23 @@ public class RegistryManager {
 		items.add(ingot_lead = new ItemBase("ingot_lead",true));
 		items.add(ingot_silver = new ItemBase("ingot_silver",true));
 		items.add(ingot_dawnstone = new ItemBase("ingot_dawnstone",true));
+		// Gloomshroud - Add Mithril Ingot
+		items.add(ingot_mithril = new ItemBase("ingot_mithril", true));
 		items.add(nugget_iron = new ItemBase("nugget_iron",true));
 		items.add(nugget_copper = new ItemBase("nugget_copper",true));
 		items.add(nugget_lead = new ItemBase("nugget_lead",true));
 		items.add(nugget_silver = new ItemBase("nugget_silver",true));
 		items.add(nugget_dawnstone = new ItemBase("nugget_dawnstone",true));
+		// Gloomshroud - Add Mithril Nugget
+		items.add(nugget_mithril = new ItemBase("nugget_mithril", true));
 		items.add(plate_copper = new ItemBase("plate_copper",true));
 		items.add(plate_lead = new ItemBase("plate_lead",true));
 		items.add(plate_silver = new ItemBase("plate_silver",true));
 		items.add(plate_dawnstone = new ItemBase("plate_dawnstone",true));
 		items.add(plate_iron = new ItemBase("plate_iron",true));
 		items.add(plate_gold = new ItemBase("plate_gold",true));
+		// Gloomshroud - Add Mithril Plate
+		items.add(plate_mithril = new ItemBase("plate_mithril", true));
 		items.add(brick_caminite = new ItemBase("brick_caminite",true));
 		items.add(blend_caminite = new ItemBase("blend_caminite",true));
 		items.add(plate_caminite = new ItemBase("plate_caminite",true));
