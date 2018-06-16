5b8fa85cfbe00b37c7cd598ac520aa02e377f0fb
BordListian <bordlistian@hotmail.de>
2018-06-16 15:51:07 +0200

    - Fixes Codex not making a sound when opened

--- src/main/java/teamroots/embers/item/ItemCodex.java
+++ src/main/java/teamroots/embers/item/ItemCodex.java
@@ -1,16 +1,14 @@
 package teamroots.embers.item;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.world.World;
 import teamroots.embers.Embers;
-import teamroots.embers.util.EmberInventoryUtil;
-import teamroots.embers.world.EmberWorldData;
+import teamroots.embers.SoundManager;
 
 public class ItemCodex extends ItemBase {
 
@@ -21,7 +19,8 @@ public class ItemCodex extends ItemBase {
 
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand){
+		world.playSound(null,player.posX, player.posY, player.posZ, SoundManager.CODEX_OPEN, SoundCategory.MASTER, 1.0f, 1.0f);
 		player.openGui(Embers.instance, 0, world, player.getPosition().getX(), player.getPosition().getY(), player.getPosition().getZ());
-		return new ActionResult<ItemStack>(EnumActionResult.PASS,player.getHeldItem(hand));
+		return new ActionResult<>(EnumActionResult.PASS, player.getHeldItem(hand));
 	}
 }
