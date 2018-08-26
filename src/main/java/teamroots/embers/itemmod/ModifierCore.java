5216846b2cd43447968be6e61e378f1654f7d208
BordListian <bordlistian@hotmail.de>
2018-08-26 02:20:53 +0200

    - Makes Core modifier not render in the tooltip

--- src/main/java/teamroots/embers/itemmod/ModifierCore.java
+++ src/main/java/teamroots/embers/itemmod/ModifierCore.java
@@ -6,5 +6,6 @@ public class ModifierCore extends ModifierBase {
 	public ModifierCore() {
 		super(EnumType.ALL,"core",0.0,false);
 		this.canRemove = false;
+		this.shouldRenderTooltip = false;
 	}
 }
