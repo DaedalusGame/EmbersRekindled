bc47b1cf468b8e7f749af50cb5fe8c2927154a6d
BordListian <bordlistian@hotmail.de>
2018-11-28 17:06:13 +0100

    - Changes Unsocket recipe to require slots for the extracted gems

--- src/main/java/teamroots/embers/recipe/AshenCloakUnsocketRecipe.java
+++ src/main/java/teamroots/embers/recipe/AshenCloakUnsocketRecipe.java
@@ -9,70 +9,71 @@ import net.minecraftforge.registries.IForgeRegistryEntry;
 import teamroots.embers.RegistryManager;
 import teamroots.embers.api.item.IInflictorGemHolder;
 
-import java.util.Collections;
-
 public class AshenCloakUnsocketRecipe extends IForgeRegistryEntry.Impl<IRecipe> implements IRecipe {
 
-	@Override
-	public boolean matches(InventoryCrafting inv, World worldIn) {
-		ItemStack cloak = ItemStack.EMPTY;
-		int cloaks = 0;
-		if (inv.getSizeInventory() > 4){
-			for (int i = 0; i < inv.getSizeInventory(); i ++){
-				ItemStack stack = inv.getStackInSlot(i);
-				if (!stack.isEmpty()){
-					if (stack.getItem() instanceof IInflictorGemHolder){
-						if (((IInflictorGemHolder) stack.getItem()).getAttachedGemCount(stack) > 0){
-							cloak = stack;
-						}
-						cloaks++;
-					}
-					else {
-						return false;
-					}
-				}
-			}
-		}
-		return !cloak.isEmpty() && cloaks == 1;
-	}
+    @Override
+    public boolean matches(InventoryCrafting inv, World worldIn) {
+        ItemStack cloak = ItemStack.EMPTY;
+        int cloaks = 0;
+        if (inv.getSizeInventory() > 4) {
+            for (int i = 0; i < inv.getSizeInventory(); i++) {
+                ItemStack stack = inv.getStackInSlot(i);
+                if (!stack.isEmpty()) {
+                    if (stack.getItem() instanceof IInflictorGemHolder) {
+                        if (((IInflictorGemHolder) stack.getItem()).getAttachedGemCount(stack) > 0) {
+                            cloak = stack;
+                        }
+                        cloaks++;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return !cloak.isEmpty() && cloaks == 1 && inv.getSizeInventory() >= ((IInflictorGemHolder) cloak.getItem()).getAttachedGemCount(cloak);
+    }
 
-	@Override
-	public ItemStack getCraftingResult(InventoryCrafting inv) {
-		ItemStack capeStack = ItemStack.EMPTY;
-		for (int i = 0; i < inv.getSizeInventory(); i ++){
-			if (!inv.getStackInSlot(i).isEmpty() && inv.getStackInSlot(i).getItem() instanceof IInflictorGemHolder) {
-				capeStack = inv.getStackInSlot(i).copy();
-			}
-		}
-		if (!capeStack.isEmpty()){
-			((IInflictorGemHolder)capeStack.getItem()).clearGems(capeStack);
-		}
-		return capeStack;
-	}
+    @Override
+    public ItemStack getCraftingResult(InventoryCrafting inv) {
+        ItemStack capeStack = ItemStack.EMPTY;
+        for (int i = 0; i < inv.getSizeInventory(); i++) {
+            if (!inv.getStackInSlot(i).isEmpty() && inv.getStackInSlot(i).getItem() instanceof IInflictorGemHolder) {
+                capeStack = inv.getStackInSlot(i).copy();
+            }
+        }
+        if (!capeStack.isEmpty()) {
+            ((IInflictorGemHolder) capeStack.getItem()).clearGems(capeStack);
+        }
+        return capeStack;
+    }
 
-	@Override
-	public ItemStack getRecipeOutput() {
-		return new ItemStack(RegistryManager.ashen_cloak_chest,1);
-	}
+    @Override
+    public ItemStack getRecipeOutput() {
+        return new ItemStack(RegistryManager.ashen_cloak_chest, 1);
+    }
 
-	@Override
-	public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv) {
-		NonNullList<ItemStack> gems = NonNullList.create();
-		for (int i = 0; i < inv.getSizeInventory(); i ++){
-			ItemStack stack = inv.getStackInSlot(i);
-			if (!stack.isEmpty()){
-				if (stack.getItem() instanceof IInflictorGemHolder){
-					Collections.addAll(gems, ((IInflictorGemHolder) stack.getItem()).getAttachedGems(stack));
-				}
-			}
-		}
-		//inv.clear();
-		return gems;
-	}
+    @Override
+    public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv) {
+        NonNullList<ItemStack> gems = NonNullList.create();
+        for (int i = 0; i < inv.getSizeInventory(); i++) {
+            ItemStack stack = inv.getStackInSlot(i);
+            if (!stack.isEmpty()) {
+                if (stack.getItem() instanceof IInflictorGemHolder) {
+                    for (ItemStack gem : ((IInflictorGemHolder) stack.getItem()).getAttachedGems(stack)) {
+                        if (!gem.isEmpty()) {
+                            gems.add(gem);
+                        }
+                    }
+                }
+            }
+        }
+        //inv.clear();
+        return gems;
+    }
 
-	@Override
-	public boolean canFit(int width, int height) {
-		return width * height >= 1;
-	}
+    @Override
+    public boolean canFit(int width, int height) {
+        return width * height >= 1;
+    }
 
 }
