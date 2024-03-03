12558f2b8fab78a558669e86f79944bbe0c9bcf6
JoshieGemFinder <79513611+JoshieGemFinder@users.noreply.github.com>
2024-03-03 20:08:48 +1000

    Add setFilter to IFilterItem.java
    Made it a default method so as to not cause issues with addons

--- src/main/java/teamroots/embers/api/item/IFilterItem.java
+++ src/main/java/teamroots/embers/api/item/IFilterItem.java
@@ -1,8 +1,14 @@
 package teamroots.embers.api.item;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import teamroots.embers.api.filter.IFilter;
 
 public interface IFilterItem {
     IFilter getFilter(ItemStack stack);
+
+	default void setFilter(ItemStack stack, IFilter filter) {
+        NBTTagCompound compound = stack.getOrCreateSubCompound("filter");
+        filter.writeToNBT(compound);
+	}
 }
